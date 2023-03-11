519c02b8b314c5282a0e3305241909b94eb6e1b9
gurrutia <durru7@gmail.com>
2023-03-10 22:39:15 -0500

    added non-terminal tests for positive_float

--- tests/validators_test.py
+++ tests/validators_test.py
@@ -3,7 +3,9 @@ import argparse
 import pytest
 
 import videoxt.constants as C
-from videoxt.validators import ValidationException, positive_int
+from videoxt.validators import positive_float
+from videoxt.validators import positive_int
+from videoxt.validators import ValidationException
 
 
 def test_positive_int_with_positive_ints():
@@ -20,26 +22,44 @@ def test_positive_int_with_valid_floats():
     assert positive_int("100_000_000.0") == 100_000_000
 
 
+def test_positive_float_with_positive_floats():
+    assert positive_float(1.0) == 1.0
+    assert positive_float("1.0") == 1.0
+    assert positive_float(12.34) == 12.34
+    assert positive_float("12.34") == 12.34
+    assert positive_float(100_000_000.0) == 100_000_000.0
+    assert positive_float("100_000_000.0") == 100_000_000.0
+
+
+def test_positive_float_with_positive_ints():
+    assert positive_float(1) == 1.0
+    assert positive_float("1") == 1.0
+    assert positive_float(100_000_000) == 100_000_000.0
+    assert positive_float("100_000_000") == 100_000_000.0
+
+
 class TestNonTerminal:
     C.IS_TERMINAL = False
 
     def test_positive_int_with_zero_from_non_terminal(self):
         with pytest.raises(ValidationException):
             positive_int(0)
+
+    def test_positive_int_with_zero_string_from_non_terminal(self):
         with pytest.raises(ValidationException):
             positive_int("0")
 
     def test_positive_int_with_negative_ints_from_non_terminal(self):
         with pytest.raises(ValidationException):
             positive_int(-1)
+
+    def test_positive_int_with_negative_ints_string_from_non_terminal(self):
         with pytest.raises(ValidationException):
             positive_int("-1")
 
-    def test_positive_int_with_non_ints_from_non_terminal(self):
+    def test_positive_int_with_non_int_string_from_non_terminal(self):
         with pytest.raises(ValidationException):
             positive_int("a")
-        with pytest.raises(ValidationException):
-            positive_int("1a")
 
     def test_positive_int_with_invalid_floats_from_non_terminal_1_1(self):
         with pytest.raises(ValidationException):
@@ -56,3 +76,19 @@ class TestNonTerminal:
     def test_positive_int_with_valid_floats_from_non_terminal_0_9_string(self):
         with pytest.raises(ValidationException):
             positive_int("0.9")
+
+    def test_positive_float_with_zero_from_non_terminal(self):
+        with pytest.raises(ValidationException):
+            positive_float(0)
+
+    def test_positive_float_with_zero_string_from_non_terminal(self):
+        with pytest.raises(ValidationException):
+            positive_float("0")
+
+    def test_positive_float_with_negative_floats_from_non_terminal(self):
+        with pytest.raises(ValidationException):
+            positive_float(-1.0)
+
+    def test_positive_float_with_negative_floats_string_from_non_terminal(self):
+        with pytest.raises(ValidationException):
+            positive_float("-1.0")

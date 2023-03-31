3eeba2ff3cc91c523c59f0163ba8227769970b2d
gurrutia <durru7@gmail.com>
2023-03-30 21:39:45 -0400

    refactor utils

--- videoxt/utils.py
+++ videoxt/utils.py
@@ -1,25 +1,50 @@
 import datetime
-from typing import Union
+import typing as t
+from pathlib import Path
 
 
-def timestamp_to_seconds(timestamp: str) -> int:
-    """Converts a timestamp string to seconds. Milliseconds are truncated."""
-    if "." in timestamp:
-        timestamp = timestamp.split(".")[0]
-
-    if ":" in timestamp:
-        seconds = 0
-        for value in timestamp.split(":"):
-            seconds = seconds * 60 + int(value)
-    else:
-        seconds = int(timestamp)
+def timestamp_to_seconds(timestamp: str) -> float:
+    """Converts a timestamp string to seconds. Microseconds are truncated."""
+    timestamp = timestamp.split(".")[0]
+    parts = timestamp.split(":")
+    seconds = sum(float(part) * 60**exp for exp, part in enumerate(reversed(parts)))
 
     return seconds
 
 
-def seconds_to_timestamp(seconds: Union[float, int]) -> str:
-    """Converts seconds to a timestamp string. Milliseconds are truncated."""
+def seconds_to_timestamp(seconds: float) -> str:
+    """Converts seconds to a timestamp string."""
     if seconds < 0:
-        seconds = 0
+        return "0:00:00"
 
     return str(datetime.timedelta(seconds=int(seconds)))
+
+
+def enumerate_dir(dir_path: Path) -> Path:
+    if not dir_path.exists():
+        return dir_path
+
+    index = 2
+    while True:
+        new_dir_path = dir_path.with_name(f"{dir_path.name} ({index})")
+        if not new_dir_path.exists():
+            return new_dir_path
+        index += 1
+
+
+def enumerate_filepath(filepath: Path) -> Path:
+    """Enumerates a Path object filepath if it already exists."""
+    if not filepath.exists():
+        return filepath
+
+    index = 2
+    while True:
+        new_filepath = filepath.with_name(f"{filepath.stem} ({index}){filepath.suffix}")
+        if not new_filepath.exists():
+            return new_filepath
+        index += 1
+
+
+def parse_kwargs(kwargs: t.Dict[str, t.Any], cls: t.Type[t.Any]) -> t.Dict[str, t.Any]:
+    """Parse kwargs and return only the keywords that match a given class."""
+    return {k: v for k, v in kwargs.items() if k in cls.__dataclass_fields__}

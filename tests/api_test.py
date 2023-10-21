afbf2f7c8feec170d50fd37a78db9fe41d66fb76
gurrutia <durru7@gmail.com>
2023-10-21 17:41:57 -0400

    Update api_test for mypy

--- tests/api_test.py
+++ tests/api_test.py
@@ -1,20 +1,21 @@
 import shutil
+from collections.abc import Generator
+from pathlib import Path
 from typing import Any
 
 import pytest
 
 from videoxt.api import extract_audio, extract_clip, extract_frames, extract_gif
 from videoxt.result import Result
-from videoxt.utils import parse_kwargs
-from videoxt.video import Video
 
 
 @pytest.fixture(scope="session")
-def extracted_frames(fixture_tmp_video_filepath) -> Result:
+def extracted_frames(fixture_tmp_video_filepath) -> Generator[Result, None, None]:
     """Extract frames from a video and yield a Result object."""
     result = extract_frames(fixture_tmp_video_filepath)
     yield result
-    shutil.rmtree(result.destpath)
+    if result.destpath is not None:
+        shutil.rmtree(Path(result.destpath))
 
 
 def test_extract_frames_valid_default_request_success_is_true(
@@ -27,16 +28,18 @@ def test_extract_frames_valid_default_request_success_is_true(
         fixture_tmp_video_properties["video_file_path"].parent / "tmp.video.mp4_frames"
     )
     assert extracted_frames.destpath == expected_destpath
-    assert extracted_frames.destpath.exists()
-    assert extracted_frames.destpath.is_dir()
+    expected_destpath = Path(expected_destpath)
+    assert expected_destpath.exists()
+    assert expected_destpath.is_dir()
 
 
 @pytest.fixture(scope="session")
-def extracted_audio(fixture_tmp_video_filepath) -> Result:
+def extracted_audio(fixture_tmp_video_filepath) -> Generator[Result, None, None]:
     """Extract audio from a video and yield a Result object."""
     result = extract_audio(fixture_tmp_video_filepath)
     yield result
-    result.destpath.unlink()
+    if result.destpath is not None:
+        Path(result.destpath).unlink()
 
 
 def test_extract_audio_valid_default_request_success_is_true(
@@ -49,16 +52,18 @@ def test_extract_audio_valid_default_request_success_is_true(
         fixture_tmp_video_properties["video_file_path"].parent / "tmp.video.mp3"
     )
     assert extracted_audio.destpath == expected_destpath
-    assert extracted_audio.destpath.exists()
-    assert extracted_audio.destpath.is_file()
+    expected_destpath = Path(expected_destpath)
+    assert expected_destpath.exists()
+    assert expected_destpath.is_file()
 
 
 @pytest.fixture(scope="session")
-def extracted_clip(fixture_tmp_video_filepath) -> Result:
+def extracted_clip(fixture_tmp_video_filepath) -> Generator[Result, None, None]:
     """Extract a clip from a video and yield a Result object."""
     result = extract_clip(fixture_tmp_video_filepath, filename="tmp.test.extract.clip")
     yield result
-    result.destpath.unlink()
+    if result.destpath is not None:
+        Path(result.destpath).unlink()
 
 
 def test_extract_clip_valid_default_request_success_is_true(
@@ -72,16 +77,18 @@ def test_extract_clip_valid_default_request_success_is_true(
         / "tmp.test.extract.clip.mp4"
     )
     assert extracted_clip.destpath == expected_destpath
-    assert extracted_clip.destpath.exists()
-    assert extracted_clip.destpath.is_file()
+    expected_destpath = Path(expected_destpath)
+    assert expected_destpath.exists()
+    assert expected_destpath.is_file()
 
 
 @pytest.fixture(scope="session")
-def extracted_gif(fixture_tmp_video_filepath) -> Result:
+def extracted_gif(fixture_tmp_video_filepath) -> Generator[Result, None, None]:
     """Extract a gif from a video and yield a Result object."""
     result = extract_gif(fixture_tmp_video_filepath)
     yield result
-    result.destpath.unlink()
+    if result.destpath is not None:
+        Path(result.destpath).unlink()
 
 
 def test_extract_gif_valid_default_request_success_is_true(
@@ -94,5 +101,6 @@ def test_extract_gif_valid_default_request_success_is_true(
         fixture_tmp_video_properties["video_file_path"].parent / "tmp.video.gif"
     )
     assert extracted_gif.destpath == expected_destpath
-    assert extracted_gif.destpath.exists()
-    assert extracted_gif.destpath.is_file()
+    expected_destpath = Path(expected_destpath)
+    assert expected_destpath.exists()
+    assert expected_destpath.is_file()

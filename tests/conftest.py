e45ec1fdc5756d90969bdc2e3f8b29937da4e52d
gurrutia <durru7@gmail.com>
2023-10-21 17:26:31 -0400

    Rewrite of conftest fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,22 +1,26 @@
-"""Configuration file containing shared pytest fixtures."""
+"""Pytest configuration file containing shared fixtures."""
 import os
 import shutil
-import typing as t
+from collections.abc import Generator
+from datetime import timedelta
 from pathlib import Path
+from typing import Any
 
-import cv2
+import cv2  # type: ignore
 import numpy as np
 import pytest
-from moviepy.editor import AudioClip
-from moviepy.editor import VideoClip
+from moviepy.editor import AudioClip, VideoClip  # type: ignore
 
 
 @pytest.fixture(scope="session")
-def session_tmp_dir() -> Path:
-    """Create a temporary directory for the session and yield its path.
+def fixture_tmp_dir() -> Generator[Path, None, None]:
+    """
+    Create a temporary directory for the session and yield its path.
 
     Yields:
-        Path: The path of the temporary directory.
+    -----
+        `Generator[Path, None, None]`:
+            A generator that yields the path to the temporary directory.
     """
     temp_dir = Path("tmp")
     temp_dir.mkdir(exist_ok=True)
@@ -25,48 +29,64 @@ def session_tmp_dir() -> Path:
 
 
 @pytest.fixture(scope="session")
-def video_properties(session_tmp_dir: Path) -> t.Dict[str, t.Any]:
-    """Create a dictionary of properties for a temporary video. Only some properties are
-    used to create the video. The rest are used to test for equality.
+def fixture_tmp_video_properties(fixture_tmp_dir: Path) -> dict[str, Any]:
+    """
+    Returns a dictionary of video properties containing values used to create a valid
+    temporary video file in `tmp_video_filepath`, and used as expected values for tests.
 
     Args:
-        session_tmp_dir (Path): The path of the temporary directory for the session.
+    -----
+        `fixture_tmp_dir` (pathlib.Path):
+            A fixture that creates a temporary directory for the session.
 
-    Yields:
-        t.Dict[str, t.Any]: A dictionary of properties for the temporary video.
+    Returns:
+    -----
+        `dict[str, Any]`: A dictionary of properties for the temporary video.
     """
     return {
-        "video_file_path": session_tmp_dir / "tmp.video.mp4",
+        "video_file_path": fixture_tmp_dir / "tmp.video.mp4",
         "filename": "tmp.video",
-        "frame_width": 640,
-        "frame_height": 480,
-        "dimensions": (640, 480),
-        "fps": 10.0,
-        "duration_seconds": 2.0,
-        "duration_timestamp": "0:00:02",
-        "frame_count": 20,
-        "suffix": "mp4",
-        "codec": "libx264",
         "audio_codec": "aac",
+        "codec": "libx264",
+        "dimensions": (640, 480),
+        "duration": timedelta(seconds=2),
+        "duration_seconds": 2,
+        "duration_timestamp": "0:00:02",
+        "fps": 10.0,
+        "frame_count": 20,
+        "frame_height": 480,
+        "frame_width": 640,
+        "has_audio": True,
     }
 
 
 @pytest.fixture(scope="session")
-def tmp_video_filepath(video_properties: t.Dict[str, t.Any]) -> Path:
-    """Create a temporary video file and yield its path.
+def fixture_tmp_video_filepath(
+    fixture_tmp_video_properties: dict[str, Any]
+) -> Generator[Path, None, None]:
+    """
+    Create a temporary video file and yield its path.
 
     Args:
-        video_properties (t.Dict[str, t.Any]): A dictionary of properties for the temporary video.
+    -----
+        `fixture_tmp_video_properties` (dict[str, Any]):
+            A fixture that returns a dictionary of properties for the temporary video.
 
     Yields:
-        Path: Filepath of the temporary video file.
+    -----
+        `Generator[Path, None, None]`:
+            A generator that yields the path to the temporary video file.
     """
 
     def make_frame(time: float) -> np.ndarray:
-        """Generate a single video frame."""
-        frame_num = int(time * video_properties["fps"]) + 1
+        """Generate mock frames for the length of the video."""
+        frame_num = int(time * fixture_tmp_video_properties["fps"]) + 1
         frame = np.zeros(
-            (video_properties["frame_height"], video_properties["frame_width"], 3),
+            (
+                fixture_tmp_video_properties["frame_height"],
+                fixture_tmp_video_properties["frame_width"],
+                3,
+            ),
             dtype=np.uint8,
         )
         text = f"Frame: {frame_num}"
@@ -74,40 +94,69 @@ def tmp_video_filepath(video_properties: t.Dict[str, t.Any]) -> Path:
         return frame
 
     def make_audio(time: float) -> float:
-        """Generate synthetic audio for the length of the video."""
+        """Generate mock audio for the length of the video."""
         return np.sin(2 * np.pi * 440 * time)
 
     # Generate the video and set the audio
-    video = VideoClip(make_frame, duration=video_properties["duration_seconds"])
-    audio = AudioClip(make_audio, duration=video_properties["duration_seconds"])
+    video = VideoClip(
+        make_frame, duration=fixture_tmp_video_properties["duration_seconds"]
+    )
+    audio = AudioClip(
+        make_audio, duration=fixture_tmp_video_properties["duration_seconds"]
+    )
     video = video.set_audio(audio)
 
     # Write the video to disk
     video.write_videofile(
-        str(video_properties["video_file_path"]),
-        codec=video_properties["codec"],
-        audio_codec=video_properties["audio_codec"],
-        fps=video_properties["fps"],
+        str(fixture_tmp_video_properties["video_file_path"]),
+        codec=fixture_tmp_video_properties["codec"],
+        audio_codec=fixture_tmp_video_properties["audio_codec"],
+        fps=fixture_tmp_video_properties["fps"],
     )
 
     # Yield the filepath of the temporary video file then delete it
-    yield video_properties["video_file_path"]
+    yield fixture_tmp_video_properties["video_file_path"]
 
     # Delete the temporary video file
-    if video_properties["video_file_path"].exists():
-        os.remove(video_properties["video_file_path"])
+    if fixture_tmp_video_properties["video_file_path"].exists():
+        os.remove(fixture_tmp_video_properties["video_file_path"])
 
 
 @pytest.fixture(scope="session")
-def tmp_text_filepath(session_tmp_dir: Path) -> Path:
-    """Create a temporary text file in the temporary directory and yield its path.
-
-    `session_tmp_dir` is a fixture that creates a temporary directory for the session.
+def fixture_tmp_video_filepath_zero_seconds() -> Generator[Path, None, None]:
+    """
+    Generate a temporary 'mp4' video file and provide its path. This file is invalid
+    due to its 0-second duration, absence of audio, and lack of frames.
 
     Yields:
-        Path: Filepath of the temporary text file.
+    -----
+        `Generator[Path, None, None]`:
+            A generator that yields the path to the temporary video file.
     """
-    tmp_filepath = session_tmp_dir / "tmp.text.txt"
+    video_file_path = Path("tmp") / "tmp.invalid.video.mp4"
+
+    # Generate the video and set the audio
+    video = VideoClip(lambda t: np.zeros((480, 640, 3), dtype=np.uint8), duration=0)
+    video.write_videofile(str(video_file_path), fps=10)
+
+    yield video_file_path
+
+    # Delete the temporary video file
+    if video_file_path.exists():
+        os.remove(video_file_path)
+
+
+@pytest.fixture(scope="session")
+def fixture_tmp_text_filepath(fixture_tmp_dir: Path) -> Generator[Path, None, None]:
+    """
+    Create a temporary text file in the temporary directory and yield its path.
+
+    Yields:
+    -----
+        `Generator[Path, None, None]`:
+            A generator that yields the path to the temporary text file.
+    """
+    tmp_filepath = fixture_tmp_dir / "tmp.text.txt"
     content = "This is a temporary text file."
     with open(tmp_filepath, "w") as f:
         f.write(content)

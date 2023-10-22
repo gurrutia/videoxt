bfd6a0682378fa991e064424efaaf5171c8205ce
gurrutia <durru7@gmail.com>
2023-10-22 15:46:53 -0400

    Rewrite of preppers.py
    Notable changes:
    - TimeRange -> ExtractionRange for clarity.
    - ExtractionRange object used to prepare and validate the range.
    - prepare_filepath() deprecated, use prepare_destpath().
    - Removed many of the less complex prepare functions.

--- videoxt/preppers.py
+++ videoxt/preppers.py
@@ -1,170 +1,281 @@
-"""Functions that prepare requests for extraction."""
+"""Contains functions, objects that prepare shared request values for extraction."""
 import math
-import typing as t
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from pathlib import Path
+from typing import Any, Optional
 
 import videoxt.utils as U
+import videoxt.validators as V
 
 
 @dataclass
-class TimeRange:
-    """A dataclass to hold the time range for extraction."""
-
-    start_timestamp: str
-    start_second: float
-    start_frame: int
-    stop_timestamp: str
-    stop_second: float
-    stop_frame: int
-
-
-def prepare_time_range(
-    start_time_request: t.Union[float, str],
-    stop_time_request: t.Optional[t.Union[float, str]],
-    duration_timestamp: str,
-    duration_seconds: float,
-    video_frame_count: int,
-    fps: float,
-) -> TimeRange:
-    """Prepare a TimeRange object to use for extraction.
-
-    If a float is requested, the requested second is rounded to two decimal places,
-    then used to calculate the timestamp and frame number.
-
-    If a string representing a timestamp is requested, the timestamp is read as is,
-    then converted to seconds which is used to calculate the frame number.
-
-    If no stop time is requested, the video's length is used.
+class ExtractionRange:
     """
-    if isinstance(start_time_request, float):
-        start_time = prepare_time_request_float(start_time_request, fps)
-    else:
-        start_time = prepare_time_request_str(start_time_request, fps)
+    Container for variations of the user's requested extraction range.
 
-    if stop_time_request is None:
-        stop_time = duration_timestamp, duration_seconds, video_frame_count
-    elif isinstance(stop_time_request, float):
-        stop_time = prepare_time_request_float(stop_time_request, fps)
-    else:
-        stop_time = prepare_time_request_str(stop_time_request, fps)
+    The range is represented in seconds, as timestamps, and frame numbers. Validations
+    and preparations are run on init.
 
-    return TimeRange(
-        start_timestamp=start_time[0],
-        start_second=start_time[1],
-        start_frame=start_time[2],
-        stop_timestamp=stop_time[0],
-        stop_second=stop_time[1],
-        stop_frame=stop_time[2],
+    Attributes:
+    -----
+        `start_request` (float | int | str):
+            Start time of the extraction in seconds or as a timestamp (`HH:MM:SS`).
+        `stop_request` (float | int | str):
+            Stop time of the extraction in seconds or as a timestamp (`HH:MM:SS`).
+        `duration_seconds` (float):
+            Duration of the video in seconds.
+        `frame_count` (int):
+            Number of frames in the video.
+        `fps` (float):
+            Frames per second to use for extraction.
+
+    Usage:
+    -----
+    >>> from videoxt.preppers import ExtractionRange
+    >>> extraction_range = ExtractionRange(
+    ...     start_request=0,
+    ...     stop_request=10,
+    ...     fps=30,
+    ...     frame_count=600,
+    ...     duration_seconds=20,
+    ... )
+    >>> extraction_range.start_second
+    0
+    >>> extraction_range.stop_second
+    10
+    >>> extraction_range.start_timestamp
+    '0:00:00'
+    >>> extraction_range.stop_timestamp
+    '0:00:10'
+    >>> extraction_range.start_frame
+    0
+    >>> extraction_range.stop_frame
+    300
+    """
+
+    start_request: float | int | str
+    stop_request: float | int | str
+    duration_seconds: float = field(repr=False)
+    frame_count: int = field(repr=False)
+    fps: float = field(repr=False)
+    start_second: float = field(init=False)
+    stop_second: float = field(init=False)
+    start_timestamp: str = field(init=False)
+    stop_timestamp: str = field(init=False)
+    start_frame: int = field(init=False)
+    stop_frame: int = field(init=False)
+
+    def __post_init__(self):
+        """
+        Start procedure that prepares and validates the extraction range.
+
+        - Convert the requested start and stop times to seconds (float)
+        - Validate the range against the video's duration
+        - Convert each request second to timestamps (str) and frame numbers (int).
+        """
+        self._prepare_start_second()
+        self._prepare_stop_second()
+        self._validate_range()
+        self._prepare_start_timestamp()
+        self._prepare_stop_timestamp()
+        self._prepare_start_frame()
+        self._prepare_stop_frame()
+
+    def to_dict(self) -> dict[str, Any]:
+        """Return a dictionary of the validated extraction start and stop points."""
+        return {
+            "start_request": self.start_request,
+            "stop_request": self.stop_request,
+            "start_second": self.start_second,
+            "stop_second": self.stop_second,
+            "start_timestamp": self.start_timestamp,
+            "stop_timestamp": self.stop_timestamp,
+            "start_frame": self.start_frame,
+            "stop_frame": self.stop_frame,
+        }
+
+    def _prepare_start_second(self) -> float:
+        """Convert the start time requested to seconds (float)."""
+        self.start_second = (
+            float(self.start_request)
+            if isinstance(self.start_request, (float, int))
+            else U.timestamp_to_seconds(self.start_request)
+        )
+        return self.start_second
+
+    def _prepare_stop_second(self) -> float:
+        """Convert the stop time requested to seconds (float)."""
+        self.stop_second = (
+            float(self.stop_request)
+            if isinstance(self.stop_request, (float, int))
+            else U.timestamp_to_seconds(self.stop_request)
+        )
+        return self.stop_second
+
+    def _validate_range(self) -> tuple[float, float, float]:
+        """
+        Validate start and stop seconds against the video's duration and each other.
+
+        Returns:
+        -----
+            `tuple[float, float, float]`: Validated (start, stop, duration) in seconds.
+        """
+        start, stop, duration = V.valid_extraction_range(
+            self.start_second, self.stop_second, self.duration_seconds
+        )
+        return start, stop, duration
+
+    def _prepare_start_timestamp(self) -> str:
+        """Convert the start time requested to a timestamp (str) `HH:MM:SS`."""
+        self.start_timestamp = (
+            self.start_request
+            if isinstance(self.start_request, str)
+            else U.seconds_to_timestamp(self.start_request)
+        )
+        return self.start_timestamp
+
+    def _prepare_stop_timestamp(self) -> str:
+        """Convert the stop time requested to a timestamp (str) `HH:MM:SS`."""
+        self.stop_timestamp = (
+            self.stop_request
+            if isinstance(self.stop_request, str)
+            else U.seconds_to_timestamp(self.stop_request)
+        )
+        return self.stop_timestamp
+
+    def _prepare_start_frame(self) -> int:
+        """Convert the start time requested to a frame number (int)."""
+        self.start_frame = math.floor(self.start_second * self.fps)
+        return self.start_frame
+
+    def _prepare_stop_frame(self) -> int:
+        """Convert the stop time requested to a frame number (int)."""
+        self.stop_frame = (
+            self.frame_count
+            if self.stop_second is None
+            else math.floor(self.stop_second * self.fps)
+        )
+        return self.stop_frame
+
+
+def prepare_extraction_range(
+    start_request: float | int | str,
+    stop_request: float | int | str,
+    duration_seconds: float,
+    frame_count: int,
+    fps: float,
+) -> dict[str, Any]:
+    """
+    Return a dictionary representing a validated extraction start and stop points.
+
+    The dictionary contains variations of the requested start and stop times as seconds
+    (float), timestamps (str), and frame numbers (int); including the requested values.
+
+    Args:
+    -----
+        `start_request` (float | int | str):
+            Start time of the extraction in seconds or as a timestamp (`HH:MM:SS`).
+        `stop_request` (float | int | str):
+            Stop time of the extraction in seconds or as a timestamp (`HH:MM:SS`).
+        `duration_seconds` (float):
+            Duration of the video in seconds.
+        `frame_count` (int):
+            Number of frames in the video.
+        `fps` (float):
+            Frames per second to use for extraction.
+
+    Returns:
+    -----
+        `dict[str, Any]`: Dictionary of validated extraction start and stop points.
+    """
+    extraction_range = ExtractionRange(
+        start_request=start_request,
+        stop_request=stop_request,
+        duration_seconds=duration_seconds,
+        frame_count=frame_count,
+        fps=fps,
     )
 
-
-def prepare_time_request_float(
-    time_request: float, fps: float
-) -> t.Tuple[str, float, int]:
-    """Prepare the requested second by rounding it to two decimal places, then use it to
-    calculate the timestamp and frame number.
-    """
-    second = round(time_request, 2)
-    timestamp = U.seconds_to_timestamp(second)
-    frame_num = prepare_frame_number(second, fps)
-
-    return timestamp, second, frame_num
-
-
-def prepare_time_request_str(time_request: str, fps: float) -> t.Tuple[str, float, int]:
-    """Prepare the requested timestamp by reading it as is, then use it to calculate the
-    second and frame number.
-    """
-    timestamp = time_request
-    second = U.timestamp_to_seconds(timestamp)
-    frame_num = prepare_frame_number(second, fps)
-
-    return timestamp, second, frame_num
-
-
-def prepare_frame_number(second: float, fps: float) -> int:
-    """Prepare the start and stop frame numbers to use for extraction."""
-    return math.floor(second * fps)
-
-
-def prepare_fps(video_fps: float, request_fps: t.Optional[float]) -> float:
-    """Prepare the fps to use for extraction. If no fps is requested, the video's fps is used."""
-    return video_fps if request_fps is None else round(request_fps, 2)
-
-
-def prepare_destdir(video_dir: Path, reqeust_dir: t.Optional[Path]) -> Path:
-    """Prepare the directory to use for extraction. If no directory is requested,
-    the video's directory is used.
-    """
-    return reqeust_dir or video_dir
-
-
-def prepare_destdir_frames(
-    video_dir: Path, video_filename: str, request_dir: t.Optional[Path]
-) -> Path:
-    """Prepare the directory to use for the 'frames' extraction request. If no directory
-    is requested, a directory will be made within the video's directory.
-    """
-    if request_dir is None:
-        return U.enumerate_dir(video_dir / f"{video_filename}_frames")
-
-    return request_dir
-
-
-def prepare_filename_frames(
-    video_filename: str, request_filename: t.Optional[str]
-) -> str:
-    """Prepare the filename to use for the 'frames' extraction request. If no filename
-    is requested, the video's filename is used.
-    """
-    return request_filename or video_filename
-
-
-def prepare_filepath(
-    video_filename: str, request_filename: t.Optional[str], dir: Path, suffix: str
-) -> Path:
-    """Prepare the filepath to use for extraction requests 'audio', 'clip', and 'gif'.
-    If no filename was requested, the video's filename is used to create a new filepath.
-    """
-    if request_filename is None:
-        return U.enumerate_filepath(dir / f"{video_filename}.{suffix}")
-
-    return dir / f"{request_filename}.{suffix}"
+    return extraction_range.to_dict()
 
 
 def prepare_dimensions(
-    video_dimensions: t.Tuple[int, int],
-    request_dimensions: t.Optional[t.Tuple[int, int]],
-    resize: float = 1.0,
-) -> t.Tuple[int, int]:
-    """Prepare the dimensions to use for extraction. If no dimensions are requested,
-    the video's dimensions are used. If a resize value other than 1 was requested,
-    the dimensions are multiplied by the resize factor.
+    video_dimensions: tuple[int, int],
+    resize: float,
+    request_dimensions: Optional[tuple[int, int]] = None,
+) -> tuple[int, int]:
+    """
+    Prepare validated dimensions for extraction considering resize factor.
+
+    Args:
+    -----
+        `video_dimensions` (tuple[int, int]):
+            Dimensions of the video.
+        `resize` (float):
+            Resize factor to apply to the video.
+        `request_dimensions` (Optional[tuple[int, int]]):
+            Optional dimensions to use. Defaults to None.
+
+    Returns:
+    -----
+        `tuple[int, int]`: Dimensions to use for extraction.
     """
     dims = request_dimensions or video_dimensions
 
-    if resize != 1:
-        dims = tuple(int(dim * resize) for dim in dims)
+    if resize != 1.0:
+        dims = (int(dims[0] * resize), int(dims[1] * resize))
 
     return dims
 
 
-def prepare_images_expected(
-    start_frame: int, stop_frame: int, capture_rate: int
-) -> int:
-    """Calculate the number of images expected to be extracted. Used to create a progress bar and
-    to determine if the extraction was successful.
+def prepare_destpath(
+    video_filepath: Path,
+    suffix: str,
+    request_filename: Optional[str] = None,
+    request_dir: Optional[Path] = None,
+    overwrite: bool = False,
+) -> Path:
     """
-    return math.ceil((stop_frame - start_frame) / capture_rate)
+    Construct and return the destination path for the extracted file.
 
+    Args:
+    -----
+        `video_filepath` (pathlib.Path):
+            Path to the video file.
+        `request_suffix` (str):
+            Required file suffix to use (with or without the leading '.').
+        `request_filename` (Optional[str]):
+            Optional filename to use. Defaults to None.
+        `request_destdir` (Optional[pathlib.Path]):
+            Optional directory to save the processed file. Defaults to None.
+        `overwrite` (bool):
+            If True, allows overwriting an existing file with the same name.
+            Defaults to False.
 
-def prepare_filepath_image(
-    dir: Path, filename: str, frame_num: int, image_format: str
-) -> str:
-    """Prepare the filepath to use for the 'frames' extraction request. The filepath is created
-    by combining the directory, filename, frame number, and image format. The frame number is
-    incremented by the capture rate during extraction.
+    Returns:
+    -----
+        `pathlib.Path`: Path to the destination file.
+
+    Notes:
+    -----
+        - Uses `request_filename` with the specified `suffix` if provided; otherwise,
+            uses video file's name.
+        - Saves the file in `request_dir` if provided; otherwise, saves in the video's
+            directory.
+        - Handles overwriting: if `overwrite` is True, may overwrite existing file with
+            the same name; if False, ensures a unique filename before saving.
     """
-    image_filename = f"{filename}_{frame_num + 1}.{image_format}"
-    return str(dir / image_filename)
+    base_dir = request_dir or video_filepath.parent
+    suffix = suffix if suffix.startswith(".") else f".{suffix}"
+
+    if request_filename:
+        filename = f"{request_filename}{suffix}"
+    else:
+        filename = f"{video_filepath.stem}{suffix}"
+
+    dest_path = base_dir / filename
+
+    if overwrite is True and dest_path != video_filepath:
+        return dest_path
+    else:
+        return U.enumerate_filepath(dest_path, label="_vxt")

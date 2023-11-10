88b2f0b33a710dab7cc09d861ecadabe71a4e62d
gurrutia <durru7@gmail.com>
2023-11-09 21:10:23 -0500

    Extend test coverage of main() cli entry point
    Previously, only 'audio' positional arg was tested. Extended to 'clip', 'frames' and 'gif'.

--- tests/cli_test.py
+++ tests/cli_test.py
@@ -1,4 +1,5 @@
 import argparse
+import shutil
 
 import pytest
 
@@ -53,13 +54,18 @@ def test_execute_extraction_returns_exit_code_1_for_invalid_video_file(
     assert exit_code == 1
 
 
-def test_main_successful_extraction_returns_exit_code_0(fixture_tmp_video_filepath):
+@pytest.mark.parametrize(
+    "method, suffix", [("audio", "mp3"), ("clip", "mp4"), ("gif", "gif")]
+)
+def test_main_successful_extraction_returns_exit_code_0(
+    fixture_tmp_video_filepath, method, suffix
+):
     destdir = fixture_tmp_video_filepath.parent
     filename = "tmp.main.successful.extraction"
-    destpath = destdir / f"{filename}.mp3"
+    destpath = destdir / f"{filename}.{suffix}"
     exit_code = main(
         [
-            "audio",
+            method,
             str(fixture_tmp_video_filepath),
             "--filename",
             filename,
@@ -76,6 +82,28 @@ def test_main_successful_extraction_returns_exit_code_0(fixture_tmp_video_filepa
         pass
 
 
+def test_main_successful_frames_extraction_returns_exit_code_0(
+    fixture_tmp_video_filepath,
+):
+    destdir = fixture_tmp_video_filepath.parent / "frames"
+    destdir.mkdir(parents=True, exist_ok=True)
+    exit_code = main(
+        [
+            "frames",
+            str(fixture_tmp_video_filepath),
+            "--destdir",
+            str(destdir),
+        ]
+    )
+    assert exit_code == 0
+    assert destdir.exists()
+
+    try:
+        shutil.rmtree(destdir)
+    except FileNotFoundError:
+        pass
+
+
 def test_main_with_invalid_positional_arg_raises_argparse_argument_error_and_returns_exit_code_1(
     fixture_tmp_video_filepath,
 ):
@@ -84,30 +112,36 @@ def test_main_with_invalid_positional_arg_raises_argparse_argument_error_and_ret
         assert exit_code == 0
 
 
+@pytest.mark.parametrize("method", ["audio", "clip", "frames", "gif"])
 def test_main_with_invalid_video_file_returns_exit_code_1(
-    fixture_tmp_video_filepath_zero_seconds,
+    fixture_tmp_video_filepath_zero_seconds, method
 ):
-    exit_code = main(["audio", str(fixture_tmp_video_filepath_zero_seconds)])
+    exit_code = main([method, str(fixture_tmp_video_filepath_zero_seconds)])
     assert exit_code == 1
 
 
+@pytest.mark.parametrize("method", ["audio", "clip", "frames", "gif"])
 def test_main_with_unrecognized_option_returns_exit_code_1(
-    fixture_tmp_video_filepath,
+    fixture_tmp_video_filepath, method
 ):
     with pytest.raises(SystemExit):
-        exit_code = main(["audio", str(fixture_tmp_video_filepath), "--invalid_option"])
+        exit_code = main([method, str(fixture_tmp_video_filepath), "--invalid_option"])
         assert exit_code == 1
 
 
-def test_main_argument_type_error_returns_exit_code_1(fixture_tmp_video_filepath):
-    exit_code = main(["audio", str(fixture_tmp_video_filepath), "--start-time", "-1"])
+@pytest.mark.parametrize("method", ["audio", "clip", "frames", "gif"])
+def test_main_argument_type_error_returns_exit_code_1(
+    fixture_tmp_video_filepath, method
+):
+    exit_code = main([method, str(fixture_tmp_video_filepath), "--start-time", "-1"])
     assert exit_code == 1
 
 
-def test_main_videoxt_error_returns_exit_code_1(fixture_tmp_video_filepath):
+@pytest.mark.parametrize("method", ["audio", "clip", "frames", "gif"])
+def test_main_videoxt_error_returns_exit_code_1(fixture_tmp_video_filepath, method):
     exit_code = main(
         [
-            "audio",
+            method,
             str(fixture_tmp_video_filepath),
             "--start-time",
             "2",

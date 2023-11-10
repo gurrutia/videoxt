f255abb721e67a0fef25dc1c15ab2ecabfa43c27
gurrutia <durru7@gmail.com>
2023-11-09 19:39:29 -0500

    Add execute_extraction() tests

--- tests/cli_test.py
+++ tests/cli_test.py
@@ -1,6 +1,6 @@
 import argparse
 
-from videoxt.cli import split_cli_args
+from videoxt.cli import execute_extraction, split_cli_args
 
 
 def test_split_cli_args():
@@ -18,3 +18,34 @@ def test_split_cli_args():
     assert extraction_method == "audio"
     assert filepath == "input_file.mp4"
     assert options == {"option1": "value1", "option2": None}
+
+
+def test_execute_extraction_returns_exit_code_0_for_valid_video_file(
+    fixture_tmp_video_filepath,
+):
+    destdir = fixture_tmp_video_filepath.parent
+    filename = "tmp.execute.extraction.audio"
+    destpath = destdir / f"{filename}.mp3"
+    exit_code = execute_extraction(
+        method="audio",  # quickest extraction method
+        filepath=str(fixture_tmp_video_filepath),
+        **{"destdir": destdir, "filename": filename},
+    )
+    assert exit_code == 0
+    assert destpath.exists()
+
+    try:
+        destpath.unlink()
+    except FileNotFoundError:
+        pass
+
+
+def test_execute_extraction_returns_exit_code_1_for_invalid_video_file(
+    fixture_tmp_video_filepath_zero_seconds,
+):
+    exit_code = execute_extraction(
+        method="audio",
+        filepath=str(fixture_tmp_video_filepath_zero_seconds),
+        **{},
+    )
+    assert exit_code == 1
